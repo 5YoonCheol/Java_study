18d610b3c6b8bc22d7c47b731607054b13afef31
Yooncheol_Oh <oyc1230@gmail.com>
2025-02-11 21:42:04 +0900

    2025.02.11 생성자 - 도입

---
+++ src/construct/MemberConstruct.java
@@ -0,0 +1,14 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    MemberConstruct(String name, int age, int grade) {
+        System.out.println("Constructing " + name + " with age " + age + " and grade " + grade);
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
