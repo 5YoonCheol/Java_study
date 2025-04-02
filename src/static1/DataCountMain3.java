d16492f29763d60565215aa321f59f6c12a1aca5
Yooncheol_Oh <oyc1230@gmail.com>
2025-04-02 21:12:39 +0900

    2025.04.02 static 변수3

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -12,5 +12,17 @@ public class DataCountMain3 {
         Data3 data3 = new Data3("C");
         System.out.println("C count=" + Data3.count);
 
+        // 추가
+        // 인스턴스를 통한 접근 (추천X)
+        // 코드를 읽을 때 마치 인스턴스 변수에 접근하는 것처럼 보이기 때문에 추천하지 않는다.
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        Data3 data5 = new Data3("D");
+        System.out.println(data5.count);
+
+        // 클래스를 통한 접근
+        // 정적 변수는 클래스에서 공용으로 관리하기 때문에 클래스를 통해서 접근하는 것이 권장된다.
+        System.out.println(Data3.count);
     }
 }
