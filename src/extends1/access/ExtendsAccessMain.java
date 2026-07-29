c24ff426ade02b39daf960adad2f1e85b2b007b1
Yooncheol_Oh <oyc1230@gmail.com>
2026-07-29 17:17:00 +0900

    2026.07.29 super - 부모 참조

--- src/extends1/access/ExtendsAccessMain.java
+++ src/extends1/access/ExtendsAccessMain.java
@@ -2,7 +2,7 @@ package extends1.access;
 
 import extends1.access.child.Child;
 
-public class ExtebdsAccessMain {
+public class ExtendsAccessMain {
 
     public static void main(String[] args) {
         Child child = new Child();
